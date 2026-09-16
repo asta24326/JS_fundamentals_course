701f5d0daae8727568c18e26a24b285755ae0c47
asta24326 <asta.aidar@gmail.com>
2026-09-16 22:23:11 +0200

    feat: make payment summary interactive

--- Amazon_project/scripts/checkout/orderSummary.js
+++ Amazon_project/scripts/checkout/orderSummary.js
@@ -4,11 +4,7 @@ import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.j
 import { formatCurrency } from '../utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-
-hello();
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
 	let cartSummaryHTML = '';
@@ -121,8 +117,10 @@ export function renderOrderSummary() {
 			removeFromCart(productId);
 
 			const container = document.querySelector(`.js-cart-item-container-${productId}`);
-
 			container.remove();
+
+			// re-render View
+			renderPaymentSummary();
 		})
 	});
 
@@ -130,7 +128,10 @@ export function renderOrderSummary() {
 		element.addEventListener('click', () => {
 			const { productId, deliveryOptionId } = element.dataset; 
 			updateDeliveryOption(productId, deliveryOptionId);
+
+			// re-render View
 			renderOrderSummary();
+			renderPaymentSummary();
 		})
 	});
 }
